059e5384c502e44ec14046ba677519ae8d6ab8fe
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-11-14 11:03:39 +0000

    Get image data as png

--- Assets/VideoFeed.cs
+++ Assets/VideoFeed.cs
@@ -2,31 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VideoFeed : MonoBehaviour {
+public class VideoFeed : MonoBehaviour
+{
 
     public Renderer renderer;
-
+    public byte[] data;
+    WebCamTexture webcamTexture;
+    bool started = false;
+    Texture2D currentFrame;
     IEnumerator openWebcam()
     {
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
         if (Application.HasUserAuthorization(UserAuthorization.WebCam))
         {
-            WebCamTexture webcamTexture = new WebCamTexture();
+            webcamTexture = new WebCamTexture();
             renderer.material.mainTexture = webcamTexture;
             webcamTexture.Play();
+            yield return new WaitForSeconds(1.0f);
+            started = true;
         }
         else
         {
         }
     }
-
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         StartCoroutine(openWebcam());
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if (webcamTexture.didUpdateThisFrame)
+            {
+                currentFrame = new Texture2D(webcamTexture.width, webcamTexture.height);
+                currentFrame.SetPixels(webcamTexture.GetPixels());
+                currentFrame.Apply();
+                data = currentFrame.EncodeToPNG();
+            }
+        }
+    }
+
+    public byte[] GetFramePNG()
+    {
+        return data;
+    }
 }
